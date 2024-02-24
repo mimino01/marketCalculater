68302750460f4d3d4f3c00e4e6527c0633c0beae
DESKTOP-FLM4T9G\mimin <mimino0144@gmail.com>
2024-02-25 04:13:47 +0900

    데이터 입력 구현완

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.FileWriter;
 import java.util.*;
 import java.io.FileReader;
@@ -10,6 +11,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         {
             Scanner scanner = new Scanner(System.in);
+            String filePath = "C:\\Users\\mimin\\IdeaProjects\\DiscordDealingProject\\src\\data.txt";
+
             // 데이터 저장
             System.out.println("데이터를 입력해주세요");
             String notProcessingData = "";
@@ -17,25 +20,28 @@ public class Main {
             while (scanner.hasNextLine()) {
                 temp = scanner.nextLine();
                 notProcessingData += temp + "\n";
-                System.out.println("scanning data / data = " + notProcessingData);
-                if (temp.isEmpty()) {
+//                System.out.println("scanning data / data = " + temp);
+                if (temp.equals("자동공지 봇")) {
                     break;
                 }
             }
 
-            String filePath = "C:\\Users\\mimin\\IdeaProjects\\DiscordDealingProject\\src\\data.txt";
 
-            System.out.println("데이터 \n" + notProcessingData);
+//            System.out.println("데이터 \n" + notProcessingData);
 
             FileWriter fileWriter = new FileWriter(filePath);
             fileWriter.write(notProcessingData);
             fileWriter.close();
+
+//            System.out.println("fileWriter function done");
 
             FileReader reader = new FileReader(filePath);
 
             int ch, length = 0, i = 0, sum = 0, max = 0, min = 10000000, maxLength = 0, minLength = 0;
             int[] input = new int[1000];
             double average = 0.0, resell = 0.0;
+
+//            System.out.println("fileReader function done");
 
             // 데이터 정리
             ArrayList<Double> time = new ArrayList<>();
@@ -44,47 +50,50 @@ public class Main {
                 int a = 0;
 
                 // 시간 정보 확인
-                if (ch == 8212) {
-                    boolean afternoon = false;
-                    boolean ThisDataIsTrueData = false;
-                    for (int j = 0;j < 6;j++) {
+                if (ch == 8212) { // 시간 데이터 시작 지점 (-)
+                    boolean afternoon = false; // 오후면 true 오전이면 false
+                    boolean ThisDataIsTrueData = false; // 더미 데이터 검사 / 더미가아니면 true 더미면 false
+                    for (int j = 0;j < 6;j++) { // (-)로 부터 6번 뒤에 있는 것이 시간 데이터
                         ch = reader.read();
 //                        System.out.println(ch);
                     }
 //                    System.out.println("afternoon data check / data = " + ch);
-                    if (ch == 54980) {
+                    if (ch == 54980) { // 오후(54980) 면 true
                         afternoon = true;
                         ThisDataIsTrueData = true;
-                    } else if (ch == 51204) {
+                    } else if (ch == 51204) { // 오전(51204) 면  false
                         afternoon = false;
                         ThisDataIsTrueData = true;
-                    } else {
+                    } else { // 더미데이터면 false
                         ThisDataIsTrueData = false;
                     }
 //                    System.out.println(ThisDataIsTrueData);
-                    if (ThisDataIsTrueData) {
+//                    System.out.println("check point 0");
+                    if (ThisDataIsTrueData) { // 더미데이터가 아니면 실행
 //                    System.out.println("befor the save / data = " + ch);
-                        String timeTamp = "";
-                        if ((ch = reader.read()) == 32) {
+                        String timeTamp = ""; // 시간 데이터 임시 저장
+                        if ((ch = reader.read()) == 32) { // 스페이스 다 날려 시작지점으로 이동
 //                        System.out.println("saving / data = " + ch);
-                            while ((ch = reader.read()) != 13) {
+                            while ((ch = reader.read()) != 10) { // 다음줄이 나오기 전까지 데이터 입력받기
 //                            System.out.println("while func inside / data = " + ch);
-                                if (ch != 58) {
+                                if (ch != 58) { // : 데이터 제외 다 입력
 //                                System.out.println("time save data check / data = " + ch);
-                                    timeTamp = timeTamp + String.valueOf((char)ch);
+                                    timeTamp = timeTamp + String.valueOf((char)ch); // 데이터 임시 저장
                                 }
                             }
 //                        System.out.println("time data check / data = " + timeTamp);
-                            time.add(Double.valueOf(timeTamp));
+                            time.add(Double.valueOf(timeTamp)); // 데이터 저장
 //                        System.out.println("after the save");
                         }
                     }
 
-                    while (ch != 49) {
+//                    System.out.println("check point 1");
+                    while (ch != 49) { // 가격데이터 나올때 까지 이동
                         ch = reader.read();
 //                        System.out.println("shift data check / data = " + ch);
                         // 예외 데이터 입력
-                        if (ch == 13) {
+                        if (ch == 10) {
+//                            System.out.println("wrong data insert");
                             time.remove(time.size() - 1);
                             break;
                         }
@@ -112,6 +121,9 @@ public class Main {
                                 }
                             }
                         }
+                    } else {
+//                        System.out.println("wrong data insert");
+                        time.remove(time.size() - 1);
                     }
                 }
             }
@@ -153,7 +165,6 @@ public class Main {
 
             System.out.println("판매 희망가를 입력해주세요");
 
-            Scanner scanner = new Scanner(System.in);
             double price = Double.parseDouble(scanner.nextLine());
 
             System.out.println("판매 수량를 입력해주세요");
