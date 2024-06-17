6745563ea5e0be397105d608e172d1c77be27ac3
DESKTOP-IDUVUTT\손제훈 <mimino0144@gmail.com>
2024-06-17 15:09:03 +0900

    리엑트 프로젝트 생성

---
+++ market_analyzer/src/App.js
@@ -0,0 +1,10 @@
+import Button from './Component.js';
+import Profile from "./Component.js";
+
+function App() {
+  return (
+    <Profile/>
+  );
+}
+
+export default App;
---
+++ market_analyzer/src/Component.js
@@ -0,0 +1,44 @@
+
+function AboutPage() {
+    return (
+        <>
+            <h1>About</h1>
+            <p>Hello there.<br />How do you do?</p>
+        </>
+    );
+}
+
+export default function Button() {
+    const isRight = true;
+    return (
+        <div>
+            {isRight && <AboutPage/>}
+        </div>
+    );
+}
+
+
+ /**
+const user = {
+    name: 'Hedy Lamarr',
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize: 90,
+};
+
+export default function Profile() {
+    return (
+        <>
+            <h1>{user.name}</h1>
+            <img
+                className="avatar"
+                src={user.imageUrl}
+                alt={'Photo of ' + user.name}
+                style={{
+                    width: user.imageSize,
+                    height: user.imageSize
+                }}
+            />
+        </>
+    );
+}
+ */
